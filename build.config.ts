5a93d546ee807153c00d6759038b9f7a64ef174b
NamesMT <dangquoctrung123@gmail.com>
2025-07-03 11:18:01 +0000

    fix: missing cli build

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,15 @@
 import { defineBuildConfig } from 'obuild/config'
 
 export default defineBuildConfig({
-  entries: [{
-    type: 'bundle',
-    input: 'src/index.ts',
-  }],
+  entries: [
+    {
+      type: 'bundle',
+      input: 'src/index.ts',
+    },
+    {
+      type: 'bundle',
+      input: 'src/cli-entry.ts',
+      dts: false,
+    },
+  ],
 })
